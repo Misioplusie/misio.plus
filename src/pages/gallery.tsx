89de172fd5b68720f15fdf097690c6007c1f599b
Rafal Proszowski <paroxp@gmail.com>
2020-11-06 10:16:03 +0000

    gallery: More dynamic approach

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,33 +1,66 @@
+import { readdir } from 'fs';
+import { resolve } from 'path';
+import { promisify } from 'util';
+
 import { DateTime, Settings } from 'luxon';
+import { GetStaticProps, GetStaticPropsResult } from 'next';
+import Head from 'next/head';
 import React, { ReactElement } from 'react';
 
-import galleryFilenames from '../../public/gallery.json';
 import { Footer, Header } from '../partials';
 
 import styles from './gallery.module.scss';
 
 Settings.defaultLocale = 'pl';
 
-export default function Gallery(): ReactElement {
-  const galleryObjects = galleryFilenames.map(filename => {
+const readDir = promisify(readdir);
+
+interface GalleryElement {
+  readonly filename: string;
+  readonly timestamp: number;
+}
+
+interface GalleryProperties {
+  readonly gallery: readonly GalleryElement[];
+}
+
+export const getStaticProps: GetStaticProps = async (): Promise<GetStaticPropsResult<GalleryProperties>> => {
+  const filenames = await readDir(resolve('public', 'gallery'));
+  const elements = filenames.map(filename => {
     const parseName = filename.match('[0-9]{4}-[0-9]{2}-[0-9]{2}');
+    const timestamp = parseName ? DateTime.fromISO(parseName[0]) : DateTime.local();
 
     return {
       filename,
-      timestamp: parseName ? DateTime.fromISO(parseName[0]) : DateTime.local(),
+      timestamp: timestamp.toMillis(),
     };
-  })
-    .sort((itemA, itemB) => itemB.timestamp.toMillis() - itemA.timestamp.toMillis());
+  });
 
+  return {
+    props: {
+      gallery: elements.sort((itemA, itemB) => itemB.timestamp - itemA.timestamp),
+    },
+  };
+};
+
+export default function Gallery(props: GalleryProperties): ReactElement {
   return <>
+    <Head>
+      {props.gallery.map(item => <link
+        key={item.filename}
+        rel="preload"
+        href={`/gallery/${item.filename}`}
+        as="image" />)}
+    </Head>
+
     <Header subtitle="Galeria" page='gallery' />
 
     <main className={styles.gallery}>
-      {galleryObjects.map(item => <div key={item.filename}>
+      {props.gallery.map(item => <div key={item.filename}>
         <a href={`/gallery/${item.filename}`} target="_blank">
           <img
             src={`/gallery/${item.filename}`}
-            alt={`Zrobione ${item.timestamp.toLocaleString(DateTime.DATE_FULL)}`} />
+            alt={`Zrobione ${DateTime.fromMillis(item.timestamp).toLocaleString(DateTime.DATE_FULL)}`} />
         </a>
       </div>)}
     </main>
