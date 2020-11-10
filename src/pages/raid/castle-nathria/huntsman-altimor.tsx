f09c78650362c65f76e02507f1e476422cbda462
Rafal Proszowski <paroxp@gmail.com>
2020-11-10 23:18:33 +0000

    raid: Improve spell representation

--- src/pages/raid/castle-nathria/huntsman-altimor.tsx
+++ src/pages/raid/castle-nathria/huntsman-altimor.tsx
@@ -17,14 +17,14 @@ function Data(): ReactElement {
 
       <li>
         Luźno ustawiamy się w okół boss'a, by zredukować ilość graczy którzy oberwą {}
-        <a href="https://shadowlands.wowhead.com/spell=334404/spreadshot">
+        <a href="https://shadowlands.wowhead.com/spell=334404/spreadshot" data-wowhead="spell=334404">
           <img src="https://wow.zamimg.com/images/wow/icons/tiny/ability_hunter_runningshot.gif" />
           Spreadshot
         </a> - nie ignorujemy przy tym, innej mechaniki.
       </li>
       <li>
         Oznaczony gracz podczas inkantacji {}
-        <a href="https://shadowlands.wowhead.com/spell=335114/sinseeker">
+        <a href="https://shadowlands.wowhead.com/spell=335114/sinseeker" data-wowhead="spell=335114">
           <img src="https://wow.zamimg.com/images/wow/icons/tiny/ability_hunter_assassinate2.gif" />
           Sinseeker
         </a> powinien ustawić się tak, by nikt inny nie stał między bossem a nim.
@@ -38,15 +38,15 @@ function Data(): ReactElement {
 
     <ul>
       <li>
-        Tankowie, muszą się zmieniać co 3-4 stacki {}
-        <a href="https://shadowlands.wowhead.com/spell=334971/jagged-claws">
+        <strong>Tankowie</strong>, muszą się zmieniać co 3-4 stacki {}
+        <a href="https://shadowlands.wowhead.com/spell=334971/jagged-claws" data-wowhead="spell=334971">
           <img src="https://wow.zamimg.com/images/wow/icons/tiny/ability_druid_disembowel.gif" />
           Jagged Claws
         </a>.
       </li>
       <li>
         Stackujcie się na graczu oznaczonym {}
-        <a href="https://shadowlands.wowhead.com/spell=334939/vicious-lunge">
+        <a href="https://shadowlands.wowhead.com/spell=334939/vicious-lunge" data-wowhead="spell=334939">
           <img src="https://wow.zamimg.com/images/wow/icons/tiny/spell_druid_bloodythrash.gif" />
           Vicious Lunge
         </a>.
@@ -61,14 +61,14 @@ function Data(): ReactElement {
           <a href="https://shadowlands.wowhead.com/npc=169457/bargast">
             Bargast
           </a> z dala od bossa i używamy przeżywajek na tanku, gdy mierzeni są {}
-          <a href="https://shadowlands.wowhead.com/spell=334797/rip-soul">
+          <a href="https://shadowlands.wowhead.com/spell=334797/rip-soul" data-wowhead="spell=334797">
             <img src="https://wow.zamimg.com/images/wow/icons/tiny/ability_druid_markofursol.gif" />
             Rip Soul
           </a>.
         </li>
       <li>
-        Healerzy powinni topować adda rzucającego {}
-        <a href="https://shadowlands.wowhead.com/spell=334797/rip-soul">
+        <strong>Healerzy</strong> powinni topować adda rzucającego {}
+        <a href="https://shadowlands.wowhead.com/spell=334797/rip-soul" data-wowhead="spell=334797">
           <img src="https://wow.zamimg.com/images/wow/icons/tiny/ability_druid_markofursol.gif" />
           Rip Soul
         </a> zanim ten dotrze do boss'a.
@@ -77,33 +77,33 @@ function Data(): ReactElement {
         Przesuwamy <a href="https://shadowlands.wowhead.com/npc=169457/bargast">
           Bargast
         </a> z dala od bossa i używamy umiejętności typu crowd control  na addach które on spawni gdy inkantuje {}
-        <a href="https://shadowlands.wowhead.com/spell=334757/shades-of-bargast">
+        <a href="https://shadowlands.wowhead.com/spell=334757/shades-of-bargast" data-wowhead="spell=334757">
           <img src="https://wow.zamimg.com/images/wow/icons/tiny/inv_felhound3_shadow_mount.gif" />
           Shades of Bargast
         </a>.
       </li>
       <li>
         Kiedy addy osiągną przynajmniej 20 stacków {}
-        <a href="https://shadowlands.wowhead.com/spell=334695/destabilize">
+        <a href="https://shadowlands.wowhead.com/spell=334695/destabilize" data-wowhead="spell=334695">
           <img src="https://wow.zamimg.com/images/wow/icons/tiny/ability_rogue_findweakness.gif" />
           Destabilize
         </a>, zabijamy je, w między czasie używając umiejętności crowd control by przerywać {}
-        <a href="https://shadowlands.wowhead.com/spell=334708/deathly-roar">
+        <a href="https://shadowlands.wowhead.com/spell=334708/deathly-roar" data-wowhead="spell=334708">
           <img src="https://wow.zamimg.com/images/wow/icons/tiny/ability_creature_cursed_03.gif" />
           Deathly Roar
         </a>.
       </li>
       <ul>
         <li>
-          Tankowie, muszą się zmieniać co 3-4 stacki {}
-          <a href="https://shadowlands.wowhead.com/spell=334971/jagged-claws">
+          <strong>Tankowie</strong>, muszą się zmieniać co 3-4 stacki {}
+          <a href="https://shadowlands.wowhead.com/spell=334971/jagged-claws" data-wowhead="spell=334971">
             <img src="https://wow.zamimg.com/images/wow/icons/tiny/ability_druid_disembowel.gif" />
             Jagged Claws
           </a>.
         </li>
         <li>
           Stackujcie się na graczu oznaczonym {}
-          <a href="https://shadowlands.wowhead.com/spell=334939/vicious-lunge">
+          <a href="https://shadowlands.wowhead.com/spell=334939/vicious-lunge" data-wowhead="spell=334939">
             <img src="https://wow.zamimg.com/images/wow/icons/tiny/spell_druid_bloodythrash.gif" />
             Vicious Lunge
           </a>.
@@ -118,24 +118,24 @@ function Data(): ReactElement {
         Przesuwamy <a href="https://shadowlands.wowhead.com/npc=169458/hecutis">
           Hecutis
         </a> aby upuścić stacki {}
-        <a href="https://shadowlands.wowhead.com/spell=334860/crushing-stone">
+        <a href="https://shadowlands.wowhead.com/spell=334860/crushing-stone" data-wowhead="spell=334860">
           <img src="https://wow.zamimg.com/images/wow/icons/tiny/shaman_pvp_rockshield.gif" />
           Crushing Stone
         </a> pamiętając że to zadaje obrażenia w rajd.
       </li>
       <li>
-        Healerzy oszczędzają odpałki healerskie, na wypadek kiedy {}
+        <strong>Healerzy</strong> oszczędzają odpałki healerskie, na wypadek kiedy {}
         <a href="https://shadowlands.wowhead.com/npc=169458/hecutis">
           Hecutis
         </a> zacznie zrzucać stacki {}
-        <a href="https://shadowlands.wowhead.com/spell=334860/crushing-stone">
+        <a href="https://shadowlands.wowhead.com/spell=334860/crushing-stone" data-wowhead="spell=334860">
           <img src="https://wow.zamimg.com/images/wow/icons/tiny/shaman_pvp_rockshield.gif" />
           Crushing Stone
           </a>.
         </li>
         <li>
           Odsuń się od rajdu kiedy pod efektem {}
-          <a href="https://shadowlands.wowhead.com/spell=334852/petrifying-howl">
+          <a href="https://shadowlands.wowhead.com/spell=334852/petrifying-howl" data-wowhead="spell=334852">
             <img src="https://wow.zamimg.com/images/wow/icons/tiny/inv_sinstonegolempet.gif" />
             Petrifying Howl
           </a> i upuść Stone Shatter w mało ruchliwym miejscu.
--- src/pages/raid/castle-nathria/shriekwing.tsx
+++ src/pages/raid/castle-nathria/shriekwing.tsx
@@ -18,14 +18,14 @@ function Data(): ReactElement {
         <ul>
           <li>
             Wszyscy chowają się za tym samym filarem, podczas {}
-            <a href="https://www.wowhead.com/spell=330711/bloodcurdling-shriek">
+            <a href="https://www.wowhead.com/spell=330711/bloodcurdling-shriek" data-wowhead="spell=330711">
               <img src="https://wow.zamimg.com/images/wow/icons/tiny/spell_shadow_deathscream.gif" />
               Bloodcurdling Shriek
             </a>.
           </li>
           <li>
             Gdy Twoja postać zostanie oznaczona {}
-            <a href="https://www.wowhead.com/spell=342074/echolocation">
+            <a href="https://www.wowhead.com/spell=342074/echolocation" data-wowhead="spell=342074">
               <img src="https://wow.zamimg.com/images/wow/icons/tiny/ability_creature_cursed_05.gif" />
               Echolocation
             </a>, udaj się do krawędzi pomieszczenia by upuścić tam plamę. Ochroni to grupę przed dodatkowymi
@@ -33,17 +33,17 @@ function Data(): ReactElement {
           </li>
           <li>
             <strong>Tankowie</strong> rzucają przeżywajki zanim otrzymają {}
-            <a href="https://www.wowhead.com/spell=328857/exsanguinating-bite">
+            <a href="https://www.wowhead.com/spell=328857/exsanguinating-bite" data-wowhead="spell=328857">
               <img src="https://wow.zamimg.com/images/wow/icons/tiny/ability_racial_cannibalize.gif" />
               Exsanguinating Bite
             </a> i powinni się zmieniać po każdej inkantacji.
           </li>
           <li>
             <strong>Healerzy</strong> muszą utrzymać raid w pełni zdrowia, gdy boss rzuca {}
-            <a href="https://www.wowhead.com/spell=344111/reverberating-scream">
+            <a href="https://www.wowhead.com/spell=344111/reverberating-scream" data-wowhead="spell=344111">
               <img src="https://wow.zamimg.com/images/wow/icons/tiny/spell_arcane_mindmastery.gif" />
               Reverberating Scream
-            </a>.
+            </a> - trwa to 16 sekund.
           </li>
         </ul>
       </Difficulty>
@@ -52,7 +52,7 @@ function Data(): ReactElement {
         <ul>
           <li>
             Prowadź {}
-            <a href="https://www.wowhead.com/spell=342863/echoing-screech">
+            <a href="https://www.wowhead.com/spell=342863/echoing-screech" data-wowhead="spell=342863">
               <img src="https://wow.zamimg.com/images/wow/icons/tiny/spell_nature_wispsplode.gif" />
               Echoing Screech
             </a> do otwartej przestrzeni, i unikaj pierścieni sonicznych które emituje boss.
@@ -70,7 +70,7 @@ function Data(): ReactElement {
           <li>Unikaj pierścieni sonicznych które się odbijają po pomieszczeniu.</li>
           <li>
             Używaj filarów, aby utrzymywać kontakt wzrokowy z bossem kiedy inkantuje {}
-            <a href="https://www.wowhead.com/spell=340047/sonar-shriek">
+            <a href="https://www.wowhead.com/spell=340047/sonar-shriek" data-wowhead="spell=340047">
               <img src="https://wow.zamimg.com/images/wow/icons/tiny/spell_shadow_coneofsilence.gif" />
               Sonar Shriek
             </a>.
